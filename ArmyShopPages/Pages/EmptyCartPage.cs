9b2ca9bb2b59aace7978c13e045bbfca4583864b
lodarius <dexxa55@gmail.com>
2022-07-06 11:01:03 +0300

    added test

---
+++ ArmyShopPages/Pages/EmptyCartPage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmyShopPages.Pages
+{
+    public class EmptyCartPage
+    {
+        public static void open()
+        {
+            Driver.open(Constants.Urls.Tests.Domain);
+        }
+
+        public static void closeAdvertisment()
+        {
+            string locator = "//*[@id='sbox-btn-close']";
+            Common.waitForElementToBeVisible(locator);
+            Common.clickElement(locator);
+        }
+
+        public static void clickAirsoftButton()
+        {
+            string locator = "//*[@id='catalog']/li[17]/a";
+            Common.clickElement(locator);
+        }
+
+        public static void clickButton()
+        {
+            string locator = "//*[@id='vmMainPage']/div[2]/div/div/div/div/div/div/div[1]/div/div[2]/a";
+            Common.clickElement(locator);
+        }
+
+        public static void clickButton2()
+        {
+            string locator = "//*[@id='vmMainPage']/div[2]/div/div/div/div/div/div/div[2]/div/div[2]/a";
+            Common.clickElement(locator);
+        }
+        public static void addToCart()
+        {
+            string locator = "//*[@id='addtocart_browse_11683']/div[2]/div/input";
+            Common.clickElement(locator);
+        }
+        public static void clickCart()
+        {
+            string locator = "//*[@id='cartlink66']/div/div/div/a";
+            Common.clickElement(locator);
+        }
+        public static void clickRemoveItem()
+        {
+            string locator = "//*[@id='delete_button_0']";
+            Common.clickElement(locator);
+        }
+
+        public static string readRemovedText()
+        {
+            string locator = "//*[@id='system-message']/dd/ul/li/a";
+            return Common.getElementText(locator);
+        }
+    }
+}
---
+++ ArmyShopTest/Tests/EmptyCart.cs
@@ -0,0 +1,39 @@
+﻿using ArmyShopPages.Pages;
+using ArmyShopTest.BaseClasses;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmyShopTest.Tests
+{
+    public class EmptyCart : BaseTest
+    {
+        [SetUp]
+        public static void setup()
+        {
+            EmptyCartPage.open();
+            EmptyCartPage.closeAdvertisment();
+        }
+
+        [Test]
+
+        public static void emptyCart()
+        {
+            string expectedMessage = "Jūsų prekių krepšelis yra tuščias! « Grįžti į prekių katalogą";
+            string acctualMessage;
+
+            EmptyCartPage.clickAirsoftButton();
+            EmptyCartPage.clickButton();
+            EmptyCartPage.clickButton2();
+            EmptyCartPage.addToCart();
+            EmptyCartPage.clickCart();
+            EmptyCartPage.clickRemoveItem();
+            acctualMessage = AddToCartButtonPage.readCartMessage();
+
+            Assert.IsTrue(acctualMessage.Contains(expectedMessage));
+        }
+    }
+}
